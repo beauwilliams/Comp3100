80b504c4ff534f7aa8292903b98da24a09c3db75
beauwilliams <beau.williams@icloud.com>
2022-03-31 00:27:48 +1100

    ADDS: LRR Algorithm for week 6 prac

--- Stage1/src/Client.java
+++ Stage1/src/Client.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 class DssimClient {
   public static void main(String args[]) throws Exception {
@@ -40,24 +41,55 @@ class DssimClient {
         outputStream.flush();
         // Response
         // System.out.println("Ds-sim Response: " + inputStream.readLine());
+        //Gets the job
         String JOBN = inputStream.readLine();
         System.out.println(JOBN);
+
+        //Splits job and gets just the specs needed
         // submit time / job id / core / memory / disk
         String[] JOBN_ARRAY = JOBN.split(" ");
         String spec = JOBN_ARRAY[4] + " " + JOBN_ARRAY[5] + " " + JOBN_ARRAY[6];
+        String jobID = JOBN_ARRAY[1];
+
+        //Gets the capable servers
         outputStream.write(("GETS Capable " + spec + "\n").getBytes());
         String DATA = inputStream.readLine();
+
+        //Gets the DATA returned from GETS Capable
         System.out.println(DATA);
         String[] DATA_ARRAY = DATA.split(" ");
         outputStream.write(("OK\n").getBytes());
 
+        //Gets the servers and also saves the highest core count found
+        int highest_core = 0;
+        int numJobs = Integer.valueOf(DATA_ARRAY[1]);
         ArrayList<String> SERVER_LIST = new ArrayList<String>();
-        for (int i = 0; i < Integer.valueOf(DATA_ARRAY[1]); i++) {
+        for (int i = 0; i < numJobs; i++) {
           // System.out.println(inputStream.readLine());
           SERVER_LIST.add(inputStream.readLine());
           System.out.println(SERVER_LIST.get(i));
+          int numCores = Integer.valueOf(SERVER_LIST.get(i).split(" ")[4]);
+          if (numCores > highest_core) {
+            highest_core = numCores;
+          }
         }
+        System.out.println(highest_core);
+        outputStream.write(("OK"+"\n").getBytes());
 
+
+        //Gets a list of the largest servers for LRR
+        ArrayList<String> LARGEST_SERVER_LIST = new ArrayList<String>();
+        for (int i = 0; i < numJobs; i++) {
+          if (Integer.valueOf(SERVER_LIST.get(i).split(" ")[4]) == highest_core) {
+            LARGEST_SERVER_LIST.add(SERVER_LIST.get(i));
+          System.out.println(SERVER_LIST.get(i));
+            String[] SERVER = SERVER_LIST.get(i).split(" ");
+            outputStream.write(("SCHD " + jobID + " " + SERVER[0] + " " + SERVER[1] + "\n").getBytes());
+            System.out.println("SCHD " + jobID + " " + SERVER[0] + " " + SERVER[1]);
+            // TimeUnit.SECONDS.sleep(2);
+            outputStream.write(("OK"+"\n").getBytes());
+          }
+        }
 
         // rvc JOBN 37 0 1135 700 3800
         // inputStream.readline() //split me into array
@@ -79,7 +111,7 @@ class DssimClient {
 
 
 
-        outputStream.write(("OK\n").getBytes());
+        outputStream.write(("OK"+"\n").getBytes());
         System.out.println("Job scheduled to largest server(s) successfully");
 
 
