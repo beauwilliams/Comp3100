4a39a4af59a92566007dd9a57f419a7dcc42ec9d
beauwilliams <beau.williams@icloud.com>
2022-03-30 18:01:06 +1100

    ADDS: Capability to schedule jobs, get lists of servers

--- Stage1/src/Client.java
+++ Stage1/src/Client.java
@@ -1,12 +1,13 @@
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
 
 class DssimClient {
   public static void main(String args[]) throws Exception {
     try {
       Socket socket = new Socket("127.0.0.1", 50000);
       BufferedReader inputStream =
-          new BufferedReader(new InputStreamReader(socket.getInputStream()));
+      new BufferedReader(new InputStreamReader(socket.getInputStream()));
       DataOutputStream outputStream =
           new DataOutputStream(socket.getOutputStream());
       try {
@@ -38,12 +39,59 @@ class DssimClient {
         outputStream.write(("REDY\n").getBytes());
         outputStream.flush();
         // Response
-        System.out.println("Ds-sim Response: " + inputStream.readLine());
+        // System.out.println("Ds-sim Response: " + inputStream.readLine());
+        String JOBN = inputStream.readLine();
+        System.out.println(JOBN);
+        // submit time / job id / core / memory / disk
+        String[] JOBN_ARRAY = JOBN.split(" ");
+        String spec = JOBN_ARRAY[4] + " " + JOBN_ARRAY[5] + " " + JOBN_ARRAY[6];
+        outputStream.write(("GETS Capable " + spec + "\n").getBytes());
+        String DATA = inputStream.readLine();
+        System.out.println(DATA);
+        String[] DATA_ARRAY = DATA.split(" ");
+        outputStream.write(("OK\n").getBytes());
+
+        ArrayList<String> SERVER_LIST = new ArrayList<String>();
+        for (int i = 0; i < Integer.valueOf(DATA_ARRAY[1]); i++) {
+          // System.out.println(inputStream.readLine());
+          SERVER_LIST.add(inputStream.readLine());
+          System.out.println(SERVER_LIST.get(i));
+        }
+
+
+        // rvc JOBN 37 0 1135 700 3800
+        // inputStream.readline() //split me into array
+        // submit time / job id / core / memory / disk
+        // then send....
+        // GETS Capable 3 700 3800
+        // rcv DATA 30 24
+        // numRecords / length records
+        // schedule 1 job SCHD 0 xlarge 0
+        // outputStream.write(("OK\n").getBytes());
+
+
+
+
+        // outputStream.write(("SCHD 0 super-silk 0\n").getBytes());
+
+
+
+
+
+
+        outputStream.write(("OK\n").getBytes());
+        System.out.println("Job scheduled to largest server(s) successfully");
+
+
+
+        //WEEK 6 ONWARDS......
+        // use for loop based on numRecords
       } catch (Exception e) {
         System.err.println(e);
       }
 
-      try {
+
+      /* try {
         // Send QUIT
         outputStream.write(("QUIT\n").getBytes());
         outputStream.flush();
@@ -51,7 +99,7 @@ class DssimClient {
         System.out.println("Ds-sim Response: " + inputStream.readLine());
       } catch (Exception e) {
         System.err.println(e);
-      }
+      } */
 
       // Close the streams and sockets
       outputStream.close();
