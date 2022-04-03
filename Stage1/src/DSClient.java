b913e8bc5bc029bd323baf2fef2a5049840a015d
beauwilliams <beau.williams@icloud.com>
2022-04-03 13:38:35 +1000

    REFACTOR: Job class for job objects

--- Stage1/src/DSClient.java
+++ Stage1/src/DSClient.java
@@ -3,6 +3,7 @@ import java.net.*;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
+
 public class DSClient {
 
   public static class DSInterface {
@@ -29,12 +30,9 @@ public class DSClient {
     public final String ERROR = "ERR";
     public final String DATA = "DATA";
 
-    private String[] job;
+    private Job job;
     private String[] data;
 
-    private String[] split(String s) {
-      return s.split(" ");
-    }
 
     public DSInterface(String host, int port) {
       this.host = host;
@@ -86,7 +84,9 @@ public class DSClient {
       try {
         send(READY);
         //TODO: Split jobs into object
-        job = split(receive());
+        // System.out.println(receive());
+        job = new Job(receive());
+        // System.out.println(job.getJobID());
       } catch (IOException e) {
         System.err.println(e);
       }
@@ -112,10 +112,103 @@ public class DSClient {
       dsserver.authenticateUser();
       dsserver.getJob();
       dsserver.getCapableServers();
-      //TODO: Implement DS-Sim client
+      //TODO: Implement DS-Sim client - LRR
     } catch (Exception e) {
-      System.out.println("Server not running or not reachable");
+      System.out.println("Server not reachable or client error");
       System.exit(0);
     }
   }
+}
+
+
+
+
+
+
+
+
+
+
+
+public class Job {
+    private Integer jobID, submitTime, estRuntime, core, memory, disk;
+    private String jobName, status;
+
+    private String[] split(String s) {
+      return s.split(" ");
+    }
+
+    public Job(Integer submitTime, Integer jobID, Integer estRuntime, Integer core, Integer memory, Integer disk) {
+        this.submitTime = submitTime;
+        this.estRuntime = estRuntime;
+        this.core = core;
+        this.memory = memory;
+        this.disk = disk;
+        this.status = "waiting";
+        this.jobName = "unnamed";
+    }
+
+    public Job(String[] jobInfo) {
+        this.submitTime = Integer.parseInt(jobInfo[1]);
+        this.jobID = Integer.parseInt(jobInfo[2]);
+        this.estRuntime = Integer.parseInt(jobInfo[3]);
+        this.core = Integer.parseInt(jobInfo[4]);
+        this.memory = Integer.parseInt(jobInfo[5]);
+        this.disk = Integer.parseInt(jobInfo[6]);
+        this.status = "waiting";
+        this.jobName = "unnamed";
+    }
+
+    public Job(String job) {
+        String[] jobInfo = split(job);
+        this.submitTime = Integer.parseInt(jobInfo[1]);
+        this.jobID = Integer.parseInt(jobInfo[2]);
+        this.estRuntime = Integer.parseInt(jobInfo[3]);
+        this.core = Integer.parseInt(jobInfo[4]);
+        this.memory = Integer.parseInt(jobInfo[5]);
+        this.disk = Integer.parseInt(jobInfo[6]);
+        this.status = "waiting";
+        this.jobName = "unnamed";
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setJobName(String jobName) {
+        this.jobName = jobName;
+    }
+
+    public Integer getJobID() {
+        return jobID;
+    }
+
+    public Integer getSubmitTime() {
+        return submitTime;
+    }
+
+    public Integer getEstimatedRuntime() {
+        return estRuntime;
+    }
+
+    public Integer getCore() {
+        return core;
+    }
+
+    public Integer getMemory() {
+        return memory;
+    }
+
+    public Integer getDisk() {
+        return disk;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+    public String getJobName() {
+        return jobName;
+    }
+
+
 }
